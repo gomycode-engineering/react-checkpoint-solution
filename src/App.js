412edc56d91f8fe95bfc32ca5216e10629afc6c6
Anis Melliti <ansmeliti@gmail.com>
2020-05-26 21:38:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App container'>
+      <h1>welcome to our first app</h1>
+      <div className='row justify-content-center'>
+        <form className='col-md-6 '>
+          <div className='form-group'>
+            <label htmlFor='exampleInputEmail1'>Email address</label>
+            <input
+              type='email'
+              className='form-control'
+              id='exampleInputEmail1'
+              aria-describedby='emailHelp'
+            />
+            <small id='emailHelp' className='form-text text-muted'>
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div className='form-group'>
+            <label htmlFor='exampleInputPassword1'>Password</label>
+            <input
+              type='password'
+              className='form-control'
+              id='exampleInputPassword1'
+            />
+          </div>
+          <div className='form-group form-check'>
+            <input
+              type='checkbox'
+              className='form-check-input'
+              id='exampleCheck1'
+            />
+            <label className='form-check-label' htmlFor='exampleCheck1'>
+              Check me out
+            </label>
+          </div>
+          <button type='submit' className='btn btn-primary'>
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
